e10ab2e17678482382d673c8e45de868d81097ae
Daniel Richman <main@danielrichman.co.uk>
2012-06-29 18:13:55 +0100

    Fixed irssi plugin

--- irssi_plugin.pl
+++ irssi_plugin.pl
@@ -1,3 +1,4 @@
+# irc2xmpp.pl
 # Based on hilightwin by Timo Sirainen http://static.quadpoint.org/irssi/
 
 use Irssi;
@@ -7,8 +8,8 @@ use vars qw($VERSION %IRSSI);
 $VERSION = "0.01";
 
 %IRSSI = (
-    authors	=> "Daniel Richman"
-    contact	=> "main\@danielrichman.co.uk"
+    authors	=> "Daniel Richman",
+    contact	=> "main\@danielrichman.co.uk",
     name	=> "irc2xmpp",
     description	=> "Forward hilighted messages to XMPP",
     license	=> "Public Domain",
@@ -16,24 +17,23 @@ $VERSION = "0.01";
     changed	=> "2012-06-29T17:23:00+0100"
 );
 
-my $client_sock = IO::Socket::UNIX->new (Type => SOCK_DGRAM);
-my $setting_name = $IRSSI{'name'} . '_socket'
+$setting_name = $IRSSI{'name'} . '_socket';
 Irssi::settings_add_str('misc', $setting_name, "relay_sock");
 
 sub sig_printtext {
   my ($dest, $text, $stripped) = @_;
 
-  # change to (MSGLEVEL_HILIGHT|MSGLEVEL_MSGS) to work for
-  # all privmsgs too
-  if (($dest->{level} & (MSGLEVEL_HILIGHT)) &&
+  if (($dest->{level} & (MSGLEVEL_HILIGHT|MSGLEVEL_MSGS)) &&
       ($dest->{level} & MSGLEVEL_NOHILIGHT) == 0) {
     if ($dest->{level} & MSGLEVEL_PUBLIC) {
-      $text = $dest->{target}.": ".$text;
+      $stripped = $dest->{target}.": ".$stripped;
     }
-    $text =~ s/%/%%/g;
 
-    my $server_sock = Irssi::settings_get_str($setting_name);
-    send($client_sock, $text, 0, $server_sock);
+    my $sock = IO::Socket::UNIX->new(
+        Type => SOCK_DGRAM,
+        Peer => Irssi::settings_get_str($setting_name)
+    );
+    send($sock, $stripped, 0);
   }
 }
 
